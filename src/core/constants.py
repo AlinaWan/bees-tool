3773c461a21bc2b995d20158e7ec262d3103e2f8
AlinaWan <wuangelina40@gmail.com>
2026-04-23 17:31:18 -0700

    refactor(core): use pathlib for paths in constants

--- src/core/constants.py
+++ src/core/constants.py
@@ -1,5 +1,4 @@
-import os
-import sys
+from pathlib import Path
 from typing import Final as ReadOnly, final as sealed
 
 from core.native_methods import NativeMethods
@@ -7,10 +6,10 @@ from core.native_methods import NativeMethods
 @sealed
 class Constants:
 
-    SCRIPT_DIR: ReadOnly = os.path.dirname(os.path.abspath(sys.argv[0]))
+    SCRIPT_DIR: ReadOnly = Path(__file__).parent.parent.absolute()
 
-    TARGET_PATH: ReadOnly = os.path.join(SCRIPT_DIR, 'resources', 'target.png')
-    METER_IMAGE_PATH: ReadOnly = os.path.join(SCRIPT_DIR, 'resources', 'meter.png')
+    TARGET_PATH: ReadOnly = SCRIPT_DIR / "resources" / "target.png"
+    METER_IMAGE_PATH: ReadOnly = SCRIPT_DIR / "resources" / "meter.png"
 
     SCREEN_WIDTH: ReadOnly = NativeMethods.get_screen_width()
     SCREEN_HEIGHT: ReadOnly = NativeMethods.get_screen_height()
